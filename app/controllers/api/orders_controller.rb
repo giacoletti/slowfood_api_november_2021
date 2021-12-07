5910bbe9d5075f235699ab6eab2a8e32700e4bbf
giacoletti <giacoletti92@gmail.com>
2021-12-07 14:37:55 +0100

    Adds request spec to create an order using a user id adds model spec for Order having an id and a user_id adds model spec for User having an id adds model spec for User and Order associations creates User factory and model creates migration to add reference user to orders and can't be null adds associations in User and Order models edits create route in orders_controller to be able to create an order in db and render it model and request specs going green

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,5 +1,13 @@
 class Api::OrdersController < ApplicationController
   def create
-    order = Order.new
+    # binding.pry
+    order = Order.create(order_params)
+    render json: { order: order }, status: :created
+  end
+
+  private
+
+  def order_params
+    params[:order].permit(:user_id)
   end
 end
