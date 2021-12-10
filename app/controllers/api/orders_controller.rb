fb0fea36de0da308c37e93127b744e6bb912e743
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-10 19:04:27 +0100

    generate user model migrate add_users_to_orders create models order_spec add order belongs to user add user has many orders add factory bot for user update requests spec

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,5 +1,21 @@
 class Api::OrdersController < ApplicationController
   def create
-    order = Order.new
+    order = Order.create(order_params)
+    render json: { order: order }, status: :created
+  end
+
+  private
+
+  def order_params
+    params[:order].permit(:user_id)
   end
 end
+
+# generate user model
+# migrate add_users_to_orders
+# create models order_spec
+# create models user_spec
+# add order belongs to user
+# add user has many orders
+# add factory bot for user
+# update requests spec
--- spec/requests/api/orders/create_spec.rb
+++ spec/requests/api/orders/create_spec.rb
@@ -1,10 +1,14 @@
 # use that namespace
 # Restful use the name of the resource you're dealing with in the path
-RSpec.describe "POST /api/orders" do
-subject { response }
-before do
- post '/api/orders', params: { order: { product_id: 1, user_id: 1 } }
-end
+RSpec.describe 'POST /api/orders' do
+  subject { response }
+  before do
+    post '/api/orders', params: { order: { product_id: 1, user_id: 1 } }
+  end
 
-it { is_expected.to have_http_status :created }
+  it { is_expected.to have_http_status :created }
+
+  it 'is expected to create an instance of Order' do
+    expect(Order.last).to_not eq nil
+  end
 end
